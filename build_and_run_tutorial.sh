d7bb3e709f5d09f0643aa30c5d1bbaadc10c04b9
zeedhoon <zeedhoon@gmail.com>
2023-02-12 19:28:58 +0900

    dealy 15 seconds on db installaion

--- build_and_run_tutorial.sh
+++ build_and_run_tutorial.sh
@@ -1,9 +1,10 @@
+docker-compose -f sample-app+tutorial/db/docker-compose.yml up -d postgres
+
 pushd sample-app+tutorial/tutorial
 npm install
 npm run build
 popd
 
 pushd sample-app+tutorial
-sh ./db/start_postgres.sh
 ./gradlew bootRun --console=plain
 popd
--- sample-app+tutorial/db/start_postgres.sh
+++ sample-app+tutorial/db/start_postgres.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
 DIR=`dirname $0`
+DELAY=1
+if [ ! -e "$DIR/data/postgres" ]; then
+    echo "installing DB.." $DIR/data/postgres
+    DELAY=15
+fi
 docker-compose -f $DIR/docker-compose.yml down
 docker-compose -f $DIR/docker-compose.yml up -d postgres
+sleep $DELAY
